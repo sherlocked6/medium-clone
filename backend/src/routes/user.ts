96b757b841ee5b7776e900dc6cfa6ce1be5e8f1f
sherlocked6 <saichandu.sambangi@gmail.com>
2024-08-03 21:04:57 +0530

    Changed create user to take name also as input

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -31,7 +31,8 @@ userRouter.post('/signup', async (c) => {
         const user = await prisma.user.create({
         data: {
             email: body.username,
-            password: body.password
+            password: body.password,
+            name: body.name
         }
         })
 
